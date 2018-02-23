1e0c08b2b38324f7396939541351f1d9dd13a06b
Tomek Pazio <tomasz1pazio@gmail.com>
2018-02-23 12:51:22 +0100

    first commit

---
+++ themes/gift/fronthack/README.md
@@ -0,0 +1,38 @@
+### Homepage - [fronthack.com](http://fronthack.com/)
+
+## Getting started
+
+### 1. You need to have a Node.js installed in your system.
+
+If you have not already done so, [click here for help](https://nodejs.org/en/download/package-manager/)
+
+
+### 2. Install Fronthack Command Line Interface
+
+```
+npm install -g fronthack
+```
+
+
+### 3. Create new Fronthack project in current path
+
+```
+fronthack init
+```
+
+### 4. Navigate to newly created fronthack directory and check out available commands
+
+```
+cd fronthack
+fronthack help
+```
+
+### 5. Start hacking
+
+Run `fronthack watch` to watch for changes you make in a *src* working directory. They will be authomatically compiled to a production ready *dist* directory. Use `fronthack new` command to create new Sass components.
+
+Fronthack is a CSS framework and a tool to automate your workflow. Check out [How to work with Fronthack](https://github.com/frontcraft/fronthack-repo/wiki/1.-How-to-work-with-Fronthack) wiki to learn how to use it properly.
+
+----------
+
+Development Supported by [WAAT Ltd](http://waat.eu)
---
+++ themes/gift/fronthack/gulpfile.js
@@ -0,0 +1,126 @@
+// Gather used gulp plugins
+var gulp = require('gulp');
+    rename = require('gulp-rename');
+    watch = require('gulp-watch');
+    sass = require('gulp-sass');
+    autoprefixer = require('gulp-autoprefixer');
+    styleguide = require('sc5-styleguide');
+    mustache = require('gulp-mustache');
+    server = require('gulp-server-livereload');
+
+
+// Set paths
+var paths = {
+  sass: {
+    input: 'src/sass/app.sass',
+    allfiles: 'src/sass/**/*.+(scss|sass)',
+    output: '../css'
+  },
+  mustache: {
+    input: './src/*.html',
+    allfiles: './src/**/*.{html,mustache}',
+    output: './dist'
+  },
+  styleguide: {
+    sass: [
+      'src/sass/**/*.+(scss|sass)',
+      'src/!sass/_*.+(scss|sass)'
+    ],
+    html: 'src/sass/**/*.html',
+    output: 'dist/styleguide',
+  }
+};
+
+// Define SASS compiling task
+gulp.task('sass', function () {
+  gulp.src(paths.sass.input)
+    .pipe(sass(
+      {outputStyle: 'compressed'}
+    ).on('error', sass.logError))
+    .pipe(rename('style.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(paths.sass.output))
+});
+
+// Define Mustache compiling task
+gulp.task('mustache', function() {
+  return gulp.src(paths.mustache.input)
+    .pipe(mustache())
+    .pipe(gulp.dest(paths.mustache.output));
+});
+
+// Define rendering styleguide task
+// https://github.com/SC5/sc5-styleguide#build-options
+gulp.task('styleguide:generate', function() {
+  return gulp.src(paths.styleguide.sass)
+    .pipe(styleguide.generate({
+        title: 'Fronthack styleguide',
+        server: true,
+        sideNav: true,
+        rootPath: paths.styleguide.output,
+        overviewPath: 'README.md',
+        commonClass: 'body',
+        extraHead: [
+          '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>',
+          '<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/owl.carousel.min.js"></script>',
+          '<script src="/js/components/owl-carousel.js"></script>',
+        ],
+        disableEncapsulation: true,
+        disableHtml5Mode: true
+      }))
+    .pipe(gulp.dest(paths.styleguide.output));
+});
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src([
+      'src/sass/app.sass',
+      'src/sass/styleguide-overrides.sass'
+    ])
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(paths.styleguide.output));
+});
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+// Define copying images for styleguide task
+gulp.task('images', function() {
+  gulp.src(['dist/images/**'])
+    .pipe(gulp.dest(paths.styleguide.output + '/images'));
+});
+
+// Define copying javascript for styleguide task
+gulp.task('js', function() {
+  gulp.src(['dist/js/components/**'])
+    .pipe(gulp.dest(paths.styleguide.output + '/js/components'));
+});
+
+// Define copying javascript for styleguide task
+gulp.task('webserver', function() {
+  gulp.src('dist')
+    .pipe(server({
+      livereload: true
+    }));
+});
+
+
+// Listen folders for changes and apply defined tasks
+gulp.task('default', [
+    'styleguide',
+    'sass',
+    'images',
+    'js',
+    'mustache',
+    'webserver'
+  ], function() {
+  gulp.watch([paths.sass.allfiles, paths.styleguide.html, paths.mustache.allfiles], [
+    'styleguide',
+    'sass',
+    'images',
+    'js',
+    'mustache'
+  ]);
+});
